63f76435afe34e8ba9b3c2050b60c859eefe93ce
dsyhwk1016 <kjsjsj1109@naver.com>
2022-06-08 14:46:12 +0900

    22/06/08 - 4 state 사용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import './App.css';
+import { useState } from 'react';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 
-function Header() {
-    return <header><h1><a href='/'>WWW</a></h1></header>
+function Header(props) {
+    return <header><h1><a href='/' onClick={e=>{
+        e.preventDefault();
+        props.onSelecte();
+    }}>WWW</a></h1></header>
 }
 
 function Nav(props) {
-    const list = props.data.map(e => <li key={e.id}><a href={'/read/' + e.id}>{e.title}</a></li>)
+    const list = props.data.map(e => <li key={e.id}><a href={'/read/' + e.id} onClick={event => {
+        event.preventDefault();
+        props.onSelecte(e.id);
+    }}>{e.title}</a></li>)
     return <nav>
         <ol>
             {list}
@@ -27,14 +34,29 @@ function App() {
         { id: 1, title: 'html', body: 'html is...' },
         { id: 2, title: 'css', body: 'css is...' }
     ]
+
+    const [mode,setMode] = useState('WELCOME');
+    const [id,setId] = useState(null);
+    let content = null;
+    if(mode === 'WELCOME'){
+        content = <Article title='Welcome' body='Hello, WEB!'></Article>
+    }else if(mode === 'READ'){
+        const topic = topics.filter(e=>e.id===id)[0];
+        content = <Article title={topic.title} body={topic.body}></Article>
+    }
+
     function createHandler() {
         alert('create!')
     }
+
     return (
         <div>
-            <Header></Header>
-            <Nav data={topics}></Nav>
-            <Article title='Welcome' body='Hello, WEB!'></Article>
+            <Header onSelecte={()=>{setMode('WELCOME')}}></Header>
+            <Nav data={topics} onSelecte={(id)=>{
+                setMode('READ');
+                setId(id);
+                }}></Nav>
+            {content}
             <ButtonGroup variant="outlined" aria-label="outlined button group">
                 <Button onClick={createHandler}>Create</Button>
                 <Button>Update</Button>

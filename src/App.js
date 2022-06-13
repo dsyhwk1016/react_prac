0c5ded3650fb440823266100e11897e4b90b4f02
dsyhwk1016 <kjsjsj1109@naver.com>
2022-06-13 14:34:36 +0900

    22/06/13 - 4 Create 구현

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const HeaderStyled = styled(Header)`
 `;
 
 function Header(props) {
-    return <header className={props.className}><h1><a href='/' onClick={e=>{
+    return <header className={props.className}><h1><a href='/' onClick={e => {
         e.preventDefault();
         props.onSelecte();
     }}>WWW</a></h1></header>
@@ -35,33 +35,65 @@ function Article(props) {
     </article>
 }
 
+const Create = (props) => {
+    return (
+        <article>
+            <h2>Create</h2>
+            <form onSubmit={event=>{
+                event.preventDefault();
+
+                const title = event.target.title.value;
+                const body = event.target.body.value;
+
+                props.onCreate(title, body);
+            }}>
+                <p><input name='title' type='text' placeholder='title' /></p>
+                <p><textarea name='body' placeholder='body' /></p>
+                <p><input type='submit' value='Create' /></p>
+            </form>
+        </article>
+    )
+}
+
 function App() {
-    const topics = [
+    const [topics, setTopics] = useState([
         { id: 1, title: 'html', body: 'html is...' },
         { id: 2, title: 'css', body: 'css is...' }
-    ]
+    ]);
+    const [nextId, setNextId] = useState(3);
+    const [mode, setMode] = useState('WELCOME');
+    const [id, setId] = useState(null);
 
-    const [mode,setMode] = useState('WELCOME');
-    const [id,setId] = useState(null);
     let content = null;
-    if(mode === 'WELCOME'){
+    if (mode === 'WELCOME') {
         content = <Article title='Welcome' body='Hello, WEB!'></Article>
-    }else if(mode === 'READ'){
-        const topic = topics.filter(e=>e.id===id)[0];
+    } else if (mode === 'READ') {
+        const topic = topics.filter(e => e.id === id)[0];
         content = <Article title={topic.title} body={topic.body}></Article>
+    } else if (mode === 'CREATE') {
+        content = <Create onCreate={(title, body)=>{
+            setTopics(topics => {
+                const newTopics = [...topics];
+                newTopics.push({id:nextId, title, body});
+                return newTopics;
+            });
+            setMode('READ');
+            setId(nextId);
+            setNextId(nextId => nextId+1);
+        }} />
     }
 
     function createHandler() {
-        alert('create!')
+        setMode('CREATE');
     }
 
     return (
         <div>
-            <HeaderStyled onSelecte={()=>{setMode('WELCOME')}}></HeaderStyled>
-            <Nav data={topics} onSelecte={(id)=>{
+            <HeaderStyled onSelecte={() => { setMode('WELCOME') }}></HeaderStyled>
+            <Nav data={topics} onSelecte={(id) => {
                 setMode('READ');
                 setId(id);
-                }}></Nav>
+            }}></Nav>
             {content}
             <ButtonGroup variant="outlined" aria-label="outlined button group">
                 <Button onClick={createHandler}>Create</Button>

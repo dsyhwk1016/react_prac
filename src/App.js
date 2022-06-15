820145f9080ce29416e2c7f77eccd773b8143f9f
dsyhwk1016 <kjsjsj1109@naver.com>
2022-06-15 11:52:47 +0900

    22/06/15 - 2 Route 사용하도록 READ 수정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import { useState } from 'react';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
-import { Link } from 'react-router-dom';
+import { Link, Routes, Route, useParams } from 'react-router-dom';
 import { HeaderStyled } from './HeaderStyled';
 import { Article } from './Article';
 import { Nav } from './Nav';
 import { Create } from './Create';
+
+function Read({topics}){
+    const id = Number(useParams().id);
+    const topic = topics.filter(e => e.id === id)[0];
+    return <Article title={topic.title} body={topic.body}></Article>;
+}
 
 function App() {
     const [mode, setMode] = useState('WELCOME');
@@ -15,25 +21,6 @@ function App() {
         { id: 2, title: 'css', body: 'css is...' }
     ]);
     const [nextId, setNextId] = useState(3);
-
-    let content = null;
-    if (mode === 'WELCOME') {
-        content = <Article title='Welcome' body='Hello, WEB!'></Article>
-    } else if (mode === 'READ') {
-        const topic = topics.filter(e => e.id === id)[0];
-        content = <Article title={topic.title} body={topic.body}></Article>
-    } else if (mode === 'CREATE') {
-        content = <Create onCreate={(title, body) => {
-            setTopics(topics => {
-                const newTopics = [...topics];
-                newTopics.push({ id: nextId, title, body });
-                return newTopics;
-            });
-            setId(nextId);
-            setMode('READ');
-            setNextId(nextId => nextId + 1);
-        }} />
-    }
 
     function createHandler() {
         setMode('CREATE');
@@ -51,7 +38,11 @@ function App() {
         <div>
             <HeaderStyled onSelecte={headerHandler()}></HeaderStyled>
             <Nav data={topics} onSelecte={navHandler()}></Nav>
-            {content}
+            <Routes>
+                <Route path='/' element={<Article title='Welcome' body='Hello, WEB!'></Article>} />
+                <Route path='/create' element={<Create onCreate={onCreateHandler()} />} />
+                <Route path='/read/:id' element={<Read topics={topics} />} />
+            </Routes>
             <ButtonGroup variant="outlined" aria-label="outlined button group">
                 <Button component={Link} to="/create" onClick={createHandler}>Create</Button>
                 <Button>Update</Button>
@@ -59,6 +50,19 @@ function App() {
             </ButtonGroup>
         </div>
     );
+
+    function onCreateHandler() {
+        return (title, body) => {
+            setTopics(topics => {
+                const newTopics = [...topics];
+                newTopics.push({ id: nextId, title, body });
+                return newTopics;
+            });
+            setId(nextId);
+            setMode('READ');
+            setNextId(nextId => nextId + 1);
+        };
+    }
 
     function navHandler() {
         return (id) => {
